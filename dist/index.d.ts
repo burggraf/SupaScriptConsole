5485581f20b58b4ab31a4a29573a8788495ef3ee
Mark Burggraf <markb@mantisbible.com>
2021-04-21 14:45:37 -0700

    reconfig

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,1 @@
-export default class SupaScriptConsoleLoggingService {
-    subscription: any;
-    constructor(obj: any);
-    StartSupaScriptConsoleLogging: (obj: any) => import("@supabase/supabase-js").RealtimeSubscription;
-    StopSupaScriptConsoleLogging: (subscription: any) => void;
-}
+export { SupaScriptConsoleLoggingService } from './service';
